40092b82825386c95366f48c1ce111d47cbfe767
Cristian <camilorendon2009@gmail.com>
2021-05-20 17:50:19 -0500

    Inicio la rama de los ejercicios2

--- exercise1.py
+++ exercise1.py
@@ -353,18 +353,3 @@ while contador_ext<5:
 #Se trata de simples comentarios en python
 
 
-objetivo = int(input("Escoge un numero: "))
-epsilon = 0.01
-bajo = 0.0
-alto = max(1.0, objetivo)
-respuesta = (alto+bajo) / 2
-
-while abs(respuesta**2 - objetivo) >= epsilon:
-    print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
-    if respuesta**2 < objetivo:
-        bajo = respuesta
-    else:
-        alto = respuesta
-    respuesta = (alto+bajo)/2
-
-print(f'La raiz cuadrada de {objetivo} es {respuesta}')
