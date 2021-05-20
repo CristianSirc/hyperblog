5dacb33afea4d67bd8ffac8d56a72f220c40cc20
Cristian <camilorendon2009@gmail.com>
2021-05-20 18:19:15 -0500

    Agrego algoritmo de aproximacion

--- exercise1.py
+++ exercise1.py
@@ -368,3 +368,20 @@ while abs(respuesta**2 - objetivo) >= epsilon:
     respuesta = (alto+bajo)/2
 
 print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+
+#Aproximacion de soluciones
+#Diferencia entre la realidad y la aproximacion es epsilon
+
+objetivo = int(input('Escoge un numero: '))
+epsilon = 0.01
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta<=objetivo:
+    print(abs(respuesta**2 - objetivo), respuesta)
+    respuesta+=paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada de {objetivo}')
+else:
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
