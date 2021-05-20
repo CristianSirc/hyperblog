e303b3a1d37ac025f9777327f1d6569ef866bb9b
Cristian <camilorendon2009@gmail.com>
2021-05-20 17:51:36 -0500

    Añado funciones de algunos algoritmos

--- exercise1.py
+++ exercise1.py
@@ -353,3 +353,51 @@ while contador_ext<5:
 #Se trata de simples comentarios en python
 
 
+
+def exhaustive(objetivo):
+    respuesta = 0
+
+    while respuesta**2 < objetivo:
+        #print(respuesta)
+        respuesta += 1
+
+    if respuesta**2 == objetivo:
+        print(f'La raiz cuadrada de {objetivo} con exhaustive es {respuesta}')
+    else:
+        print(f'{objetivo} no tiene una raiz cuadrada exacta')
+
+
+def aproximacion(objetivo):
+    epsilon = 0.01
+    paso = epsilon**2
+    respuesta = 0.0
+
+    while abs(respuesta**2 - objetivo) >= epsilon and respuesta<=objetivo:
+        #print(abs(respuesta**2 - objetivo), respuesta)
+        respuesta+=paso
+
+    if abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'No se encontro la raiz cuadrada de {objetivo}')
+    else:
+        print(f'La raiz cuadrada de {objetivo} con aproximacion es {respuesta}')
+
+
+def bisection(objetivo):
+    epsilon = 0.01
+    bajo = 0.0
+    alto = max(1.0, objetivo)
+    respuesta = (alto+bajo) / 2
+
+    while abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
+        if respuesta**2 < objetivo:
+            bajo = respuesta
+        else:
+            alto = respuesta
+        respuesta = (alto+bajo)/2
+
+    print(f'La raiz cuadrada de {objetivo} con bisection es {respuesta}')
+
+exhaustive(4)
+aproximacion(4)
+bisection(4)
